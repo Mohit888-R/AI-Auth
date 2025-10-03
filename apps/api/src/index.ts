37c082273f2a4fdc86e84afa6d14ccc7f081fb1f
MohitOru <141000266+MohitOru@users.noreply.github.com>
2025-10-04 00:01:16 +0530

    ml/api setup

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API running with pnpm" });
+});
+
+const PORT = process.env.SERVER_PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT} : http://localhost:${PORT}`));
